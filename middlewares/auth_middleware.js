6283cb42628d32e8676cd850054c11ecaed95a5e
rozoomcool <rozoom.um@gmail.com>
2023-10-06 01:29:52 +0300

    .

--- middlewares/auth_middleware.js
+++ middlewares/auth_middleware.js
@@ -1,6 +1,6 @@
 module.exports = checkUserRole = () => {
     return function(req, res, next) {
-      if (req.session && req.session.username) {
+      if (req.session.username) {
         next();
       } else {
         res.status(403).send('Доступ запрещен');
