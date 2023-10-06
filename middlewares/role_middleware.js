3d89e750c29169cd408724fd1fd46d985fd3a47d
rozoomcool <rozoom.um@gmail.com>
2023-10-06 09:08:01 +0300

    .

--- middlewares/role_middleware.js
+++ middlewares/role_middleware.js
@@ -1,16 +1,5 @@
 const User = require('../models/user');
 
-<<<<<<< Updated upstream
-module.exports = checkUserRole = (role) => async (req, res, next) => {
-  if (req.session && req.session.username) {
-    await User.findOne({username: req.session.username})
-      .then((el) => {
-        if (el.role === role) next();
-        else res.status(403).json({message: "Доступ запрещен!"});
-      })
-      .catch((err) => res.status(403).json({message: "Доступ запрещен!"}));
-    
-=======
 module.exports = (role) => async (req, res, next) => {
   if (req.session && req.session.username) {
     await User.findOne({username: req.session.username})
@@ -19,7 +8,6 @@ module.exports = (role) => async (req, res, next) => {
           else res.status(403).json({message: "У тебя нет прав"});
       })
       .catch((err) => res.status(403).json({message: "У тебя нет прав"}));
->>>>>>> Stashed changes
   } else {
     res.status(403).send('Доступ запрещен');
   }
