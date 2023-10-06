9810e145ec9ee13c167430ab58fac159dfe80b9d
rozoomcool <rozoom.um@gmail.com>
2023-10-06 09:05:30 +0300

    add student? teacher profile, test add page, and role_middleware

--- middlewares/role_middleware.js
+++ middlewares/role_middleware.js
@@ -1,15 +1,15 @@
-const roles = {
-  admin,
-  teacher,
-  student
-}
+const User = require('../models/user');
 
-module.exports = checkUserRole = (role) => {
-  return function(req, res, next) {
-    if (req.session && req.session.user && req.session.user.role === role) {
-      next();
-    } else {
-      res.status(403).send('Доступ запрещен');
-    }
+module.exports = checkUserRole = (role) => async (req, res, next) => {
+  if (req.session && req.session.username) {
+    await User.findOne({username: req.session.username})
+      .then((el) => {
+        if (el.role === role) next();
+        else res.status(403).json({message: "Доступ запрещен!"});
+      })
+      .catch((err) => res.status(403).json({message: "Доступ запрещен!"}));
+    
+  } else {
+    res.status(403).send('Доступ запрещен');
   }
 }
